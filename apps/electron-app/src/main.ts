59d0598560017c96d3d4cee753c62133c9663575
Core <core@coredev.uk>
2026-01-21 09:07:37 +0000

    feat(electron): add db init

--- apps/electron-app/src/main.ts
+++ apps/electron-app/src/main.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
 import { app, BrowserWindow } from "electron";
+import { initDB } from "./db";
 
 const createWindow = () => {
   // Create the browser window.
@@ -27,7 +28,15 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on("ready", () => {
+  try {
+    initDB();
+    console.log("Database initialized successfully");
+    createWindow();
+  } catch (e) {
+    console.error("Failed to boot DB", e);
+  }
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
